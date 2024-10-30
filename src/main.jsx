6e5e3f1f2d1cb2c1ffe0fe5dfd52ceb2395945f9
@maptaul <maptaulislam1@gmail.com>
2024-10-30 11:09:58 +0600

    df

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 import "./index.css";
 import Contact from "./routes/contact";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 
 const router = createBrowserRouter([
   {
@@ -21,6 +21,13 @@ const router = createBrowserRouter([
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
   },
   {
     path: "contacts/:contactId",
